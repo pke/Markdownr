6da5d0a88ac2de632c6b47adb362479933d257ae
Philipp Kursawe <phil.kursawe@gmail.com>
2015-10-29 23:59:04 +0100

    Custom header renderer to support TOC

--- Markdownr/Markdownr.Shared/js/worker.js
+++ Markdownr/Markdownr.Shared/js/worker.js
@@ -3,7 +3,25 @@ importScripts("/lib/marked.min.js");
 
 onmessage = function (event) {
     if (event.data.content) {
-        marked(event.data.content, function (err, content) {
+        var renderer = new marked.Renderer();
+        renderer.heading = function (text, level, raw) {
+            var name = text.toLowerCase().replace(/[^\w]+/g, '-');
+            return '<h'
+                + level
+                + '><a name="'
+                + name
+                + '" class="anchor" aria-hidden="true" href="#'
+                + name
+                + '"><span class="octicon octicon-link"></span></a>'
+                + text
+                + '</h'
+                + level
+                + '>';
+        }
+        var options = {
+            renderer: renderer
+        };
+        marked(event.data.content, options, function (err, content) {
             if (err) {
                 throw err;
             } else {
